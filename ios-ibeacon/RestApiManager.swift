e3551b412896309a1e07e8abfb383712fb43499d
Sarah Kharimah <sarahkharimah@Sarahs-MacBook-Pro.local>
2016-11-16 23:24:16 -0500

    Add RestApiManager Class to take care of the HTTP requests

--- ios-ibeacon/RestApiManager.swift
+++ ios-ibeacon/RestApiManager.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+class RestApiManager {
+    
+    private var groupName = String()
+    private var canvasUrl = String()
+    
+    func getUrlWithParams(baseUrl: String,
+                          uuid: String,
+                          canvasId: String,
+                          major: (Int),
+                          minor: (Int)) -> String {
+        let urlWithParams = baseUrl + "?uuid=\(uuid)&canvas_id=\(canvasId)&major=\(major)&minor=\(minor)"
+        return urlWithParams
+    }
+    
+    func getGroupName(newGroupName: String) -> String {
+        return self.groupName
+    }
+    
+    func getCanvasUrl(newCanvasUrl: String) -> String {
+        return self.canvasUrl
+    }
+    
+    func setGroupName(newGroupName: String) {
+        self.groupName = newGroupName
+    }
+    
+    func setCanvasUrl(newCanvasUrl: String) {
+        self.canvasUrl = newCanvasUrl
+    }
+    
+    func sendHttpRequest(urlWithParams: String) {
+        
+        let urlRequest = URL(string: urlWithParams)
+        
+        URLSession.shared.dataTask(with:urlRequest!) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                do {
+                    let parsedJSON = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
+                    
+                    self.setGroupName(newGroupName: parsedJSON["group_name"] as! String)
+                    self.setCanvasUrl(newCanvasUrl: parsedJSON["url"] as! String)
+                    
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+            
+            }.resume()
+    }
+
+}
+
+
